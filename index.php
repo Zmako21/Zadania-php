58a8f86829a8f90183108502d7a9ea43d94f6d3e
Zmako21 <shadowkacper240@gmail.com>
2026-04-17 08:36:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = 26;
+echo $a . "<br>";
+echo -$a . "<br>";
+echo ($a += 3) . "<br>";
+echo ($a -= 5) . "<br>";
+echo ($a *= 2) . "<br>";
+echo ($a /= 5) . "<br>";
+echo ($a %= 2) . "<br>";
+echo ($a = 2) . "<br>";
+echo ($a=pow(26,3)) . "<br>";
+echo "Wartość zmiennej $ a wynosi: " . $a . " i już się nie zmieni";
+?>
+    
+</body>
+</html>
+
